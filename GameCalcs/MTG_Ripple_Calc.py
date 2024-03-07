12655a4c7ea24fe172d5b9ff58077390e257f163
Niko Matikainen <niko.matikainen@gmail.com>
2024-03-07 15:46:01 +0200

    Added multiprocessing to ripple calc

--- GameCalcs/MTG_Ripple_Calc.py
+++ GameCalcs/MTG_Ripple_Calc.py
@@ -3,6 +3,9 @@ import random
 import time
 from prettytable import PrettyTable
 from os import system, name
+from dataclasses import dataclass
+from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool
 
 def clear():
 	_ = system('cls') if name == 'nt' else system('clear')
@@ -20,27 +23,57 @@ correctCardsPerDeckMax = 50
 
 # ----------------------------------------------------------------------------------
 
+@dataclass
 class Result:
-	def __init__(self, hitsInDeck: int, totalCasts: int, decksToSimulate: int, runouts: int):
-		self.totalDecks = decksToSimulate
-		self.hitsInDeck = hitsInDeck
-		self.totalCasts = totalCasts
-		self.runouts = runouts
+	hitsInDeck: int
+	totalCasts: int
+	totalDecks: int
+	runouts: int
 
 	def averageCasts(self) -> float:
 		return self.totalCasts / self.totalDecks
 
 # --- Main logic -------------------------------------------------------------------
 
-timer = time.perf_counter()
-start = timer
+def main():
+	timer = time.perf_counter()
+	start = timer
 
-results = []
+	results = []
+	amounts = range(correctCardsPerDeckMin, correctCardsPerDeckMax + 1)
+	with Pool() as pool:
+		results = pool.imap(simulate_deck, amounts)
 
-for hitsInDeck in range(correctCardsPerDeckMin, correctCardsPerDeckMax + 1):
+
+		table = PrettyTable()
+		table.field_names = ["Cards in deck", "Average casts", "Chance to cast everything"]
+
+		for field_name in table.field_names:
+			table.align[field_name] = "l"
+
+		for result in results:
+			table.add_row([f"{result.hitsInDeck}", 
+						f"{round(result.averageCasts(), 2)}", 
+						f"{round((result.runouts / result.totalDecks) * 100, 2)}%"])
+
+		clear()
+		print(f"Simulation time                : {round(time.perf_counter() - start, 2)} seconds")
+		print(f"Total decks                    : {decksToSimulate}")
+		print(f"Deck size                      : {cardsPerDeck}")
+		print(f"Cards drawn at start           : {cardsDrawnAtStart}")
+		print(f"Hits in deck start             : {correctCardsPerDeckMin}")
+		print(f"Hits in deck end               : {correctCardsPerDeckMax}")
+		space()
+
+		print(table)
+
+# ----------------------------------------------------------------------------------
+	
+
+
+def simulate_deck(hitsInDeck: int) -> Result:
 	totalCasts = 0
 	runouts = 0
-
 	for deckIndex in range(decksToSimulate):
 		pool = []
 		pool.extend([True] * hitsInDeck)
@@ -78,27 +111,9 @@ for hitsInDeck in range(correctCardsPerDeckMin, correctCardsPerDeckMax + 1):
 			#print(f"Deck {deckIndex + 1}, hits in deck: {hitsInDeck}, hits in this deck: {thisDeckCasts}, stackCount {stackCount}, deck size: {len(pool)}")
 			#time.sleep(0.1)
 	
-	results.append(Result(hitsInDeck, totalCasts, decksToSimulate, runouts))
+	return Result(hitsInDeck, totalCasts, decksToSimulate, runouts)
 
 
-clear()
-print(f"Simulation time                : {round(time.perf_counter() - start, 2)} seconds")
-print(f"Total decks                    : {decksToSimulate}")
-print(f"Deck size                      : {cardsPerDeck}")
-print(f"Cards drawn at start           : {cardsDrawnAtStart}")
-print(f"Hits in deck start             : {correctCardsPerDeckMin}")
-print(f"Hits in deck end               : {correctCardsPerDeckMax}")
-space()
 
-table = PrettyTable()
-table.field_names = ["Cards in deck", "Average casts", "Chance to cast everything"]
-
-for field_name in table.field_names:
-    table.align[field_name] = "l"
-
-for result in results:
-	table.add_row([f"{result.hitsInDeck}", 
-				   f"{round(result.averageCasts(), 2)}", 
-				   f"{round((result.runouts / result.totalDecks) * 100, 2)}%"])
-
-print(table)
+if __name__ == "__main__":
+	main()
